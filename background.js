6a400a34f61e7fc2cd8d7e9ea10d9bfc4d9208af
Sebastian Lopez-Cot <sclopez2@hotmail.com>
2016-08-17 00:40:59 -0400

    Update background.js

--- background.js
+++ background.js
@@ -5,7 +5,6 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse)
     if(request.action)
     {
     	if(request.action == "init"){
-    		// Send back an array of frameIds that have google docs loaded inside
             chrome.tabs.executeScript(sender.tab.id, {
                 file: "injection.js"
             });
