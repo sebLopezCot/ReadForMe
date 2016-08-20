64ee7ff5fa2ef260320e9283027bb69072475b16
sebLopezCot <sclopez2@hotmail.com>
2016-08-20 19:37:52 -0400

    Got rid of fixed size chunking and using sentence punctuation instead

--- injection.js
+++ injection.js
@@ -1,53 +1,15 @@
 // Gets injected to the page on load
 
-String.prototype.chunk = function(charMax) {
-	// turn object into literal
-	var str = this.toString();
+if('speechSynthesis' in window){
 
-	// break into word array
-	var words = str.split(' ');
+	var msg = new SpeechSynthesisUtterance('');
 
-	// for each word, get the number of chars it plus the words
-	// before it would total up to
-	var charCount = [];
-	for(i in words){
-		var word = words[i];
-		var prevCharCount = 0;
-		if(i > 0){
-			prevCharCount = charCount[i-1];
-		}
-		charCount.push(prevCharCount + word.length + 1);
-	}
-
-	// divide into chunks of a given max length
-	var chunks = [];
-	var chunkStart = 0;
-	var chunksSoFar = 0;
-	for(i in charCount){
-		var sum = charCount[i];
-		sum -= chunksSoFar * charMax;
-
-		if(sum > charMax || i == charCount.length - 1){
-			var end = null;
-			if(i == charCount.length - 1){
-				end = i+1;
-			} else {
-				end = i;
-			}
-			chunks.push(words.slice(chunkStart, end).join(' '));
-			chunkStart = i;
-			chunksSoFar++;
-		}
-	}
-
-	return chunks;
-};
-
+}
 
 function speak(message, done){
 	if('speechSynthesis' in window && reading){
 
-		var msg = new SpeechSynthesisUtterance(message);
+		msg.text = message;
 		msg.voice = window.speechSynthesis.getVoices().filter(function(voice){ return voice.name == "Google US English"; })[0];
 		// msg.rate = 1.5;
 		msg.onend = function(){
@@ -110,7 +72,12 @@ $('h1,h2,h3,h4,h5,h6,p,blockquote').click(function(){
 
 		function performSpeech(){
 			// Turn text into chunks
-			var chunks = $(self).text().chunk(200);
+			var chunks = $(self).text()
+								.replace('?', '~')
+								.replace('!', '~')
+								.replace('.', '~')
+								.replace(',', '~')
+								.split('~');
 			reading = true;
 			chainDone = false;
 			async.eachSeries(chunks, speak, function(err) {
