052fcbcb7d74c2873ae50d2fd7448da2c797c2ad
sebLopezCot <sclopez2@hotmail.com>
2016-08-24 15:05:02 -0400

    Fixed bug with sentence parsing

--- injection.js
+++ injection.js
@@ -86,10 +86,10 @@ $('h1,h2,h3,h4,h5,h6,p,blockquote,ul').click(function(){
 			}
 
 			var chunks = chunkText
-								.replace('?', '~')
-								.replace('!', '~')
-								.replace('.', '~')
-								.replace(',', '~')
+								.replace(/\?/g, '~')
+								.replace(/\!/g, '~')
+								.replace(/\./g, '~')
+								.replace(/\,/g, '~')
 								.split('~');
 			reading = true;
 			chainDone = false;
