149d5fd94f520787849abbebf3e8aefdecb4e3ca
sebLopezCot <sclopez2@hotmail.com>
2016-08-24 14:04:26 -0400

    Works in all frames of a page

--- background.js
+++ background.js
@@ -5,9 +5,18 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse)
     if(request.action)
     {
     	if(request.action == "init"){
-            chrome.tabs.executeScript(sender.tab.id, {
-                file: "injection.js"
-            });
+            // Send back an array of frameIds that have google docs loaded inside
+    		chrome.webNavigation.getAllFrames({tabId: sender.tab.id}, function(frames){
+    			var frameIds = frames.map(function(frame){ return frame.frameId });
+    			
+    			// Inject scrollbar event handling mechanism into iframes
+				frameIds.forEach(function(frameId){
+					chrome.tabs.executeScript(sender.tab.id, {
+						file: "injection.js",
+						frameId: frameId
+					});
+				});
+    		});
     	}
     }
 });
